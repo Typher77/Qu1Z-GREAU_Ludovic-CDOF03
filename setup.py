10b7917fbbf88bf097193890e18f0e314f446bb7
Typher77 <greauludo@gmail.com>
2024-01-22 18:10:39 +0100

    Ajout de setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+
+setup(
+    name='Qu1Z-GREAU_Ludovic-CDOF03',
+    version='1.0',
+    author='Typher77',
+    license='MIT',
+    long_description=open('README.md').read()
+)
